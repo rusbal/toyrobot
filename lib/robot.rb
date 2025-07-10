cc1f24a43bf8bc1dc444d3b3a65a02f16be2ab75
Raymond Usbal <raymond@philippinedev.com>
2025-07-10 14:17:04 +0800

    place method with validation

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,10 +1,14 @@
 class Robot
+  DIRECTIONS = ['NORTH', 'SOUTH', 'EAST', 'WEST'].freeze
+
   def initialize
     @state = {}
   end
 
-  def place(x, y, direction)
-    @state = { x: x, y: y, direction: direction }
+  def place(*args)
+    if valid?(*args)
+      @state = { x: args[0], y: args[1], direction: args[2] }
+    end
   end
 
   def placed?
@@ -14,4 +18,14 @@ class Robot
   private
 
   attr_accessor :state
+
+  def valid?(*args)
+    x, y, direction = *args
+
+    valid_point = ->(n) { n >= 0 && n <= 4 }
+
+    valid_point.call(x) &&
+      valid_point.call(y) &&
+      DIRECTIONS.include?(direction)
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -11,9 +11,32 @@ RSpec.describe Robot do
     end
 
     context 'placed' do
-      it 'will return true' do
-        robot.place(0, 0, 'NORTH')
-        expect(robot.placed?).to eq true
+      context 'with valid placement' do
+        it 'will return true' do
+          robot.place(0, 0, 'NORTH')
+          expect(robot.placed?).to eq true
+        end
+      end
+
+      context 'with invalid placement x' do
+        it 'will return false' do
+          robot.place(5, 0, 'NORTH')
+          expect(robot.placed?).to eq false
+        end
+      end
+
+      context 'with invalid placement y' do
+        it 'will return false' do
+          robot.place(0, 5, 'NORTH')
+          expect(robot.placed?).to eq false
+        end
+      end
+
+      context 'with invalid placement direction' do
+        it 'will return false' do
+          robot.place(0, 0, 'AMERICA')
+          expect(robot.placed?).to eq false
+        end
       end
     end
   end
